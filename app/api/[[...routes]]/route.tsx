25fa0ea5bd78253053dc896bae61cf87518ddd36
scapula07 <bartholomewonogwu@yahoo.com>
2024-07-21 00:46:39 +0100

    add text2image api call

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -2,30 +2,71 @@
 
 import { Button, Frog, TextInput } from 'frog'
 import { devtools } from 'frog/dev'
-// import { neynar } from 'frog/hubs'
 import { handle } from 'frog/next'
 import { serveStatic } from 'frog/serve-static'
+import { createSystem } from 'frog/ui'
+import { SDAPI } from '@/app/lib'
+import { Txt2imgInput } from '@/app/lib/type'
+
+
 
 const app = new Frog({
   title:"frame",
   assetsPath: '/',
   basePath: '/api',
-  // Supply a Hub to enable frame verification.
-  // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
 })
 
-// Uncomment to use Edge Runtime
-// export const runtime = 'edge'
+
 
 app.frame('/', (c) => {
-  const { buttonValue, inputText, status } = c
-  const fruit = inputText || buttonValue
   return c.res({
+    action:'/prompt',
     image: `https://frog-frame-coral.vercel.app/background.png`,
     imageAspectRatio:'1:1',
     intents: [<Button value="">Let's Start</Button>]  
   })
 })
+
+
+app.frame('/prompt', (c) => {
+ 
+  return c.res({
+    action:'/mint',
+    image:(
+      <div
+        style={
+          {
+            color:"white",
+            fontSize:"60px"
+          }
+        }
+       >
+        This
+      </div>
+    ),
+    imageAspectRatio:'1.91:1',
+    intents: [
+      <TextInput placeholder="A cool cat on the beach..." />,
+      <Button value="">Generate</Button>
+      ]  
+  })
+})
+
+
+app.frame('/mint', async(c) => {
+  const api= new SDAPI()
+  const result = await api.txt2img(c?.frameData?.inputText || "");
+  return c.res({
+    action:'/prompt',
+    image: result?.outputs[0]?.url,
+    imageAspectRatio:'1:1',
+    intents: [
+      <Button value="">Regenerate</Button>,
+      <Button value="">Mint</Button>
+    ]  
+  })
+})
+
 
 devtools(app, { serveStatic })
 
